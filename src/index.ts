2f5d0277414e98451c927266467884180493f689
ravvi-kumar <ravvi.kumar8989@gmail.com>
2025-03-30 16:43:38 +0530

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,34 @@
 import { Elysia } from "elysia";
+import { swagger } from '@elysiajs/swagger'
+import  { cors } from '@elysiajs/cors'
+import betterAuthView from "./lib/auth-view";
+import { auth } from "./lib/auth";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+.use(cors()).use(swagger())
+.all("/api/auth/*", betterAuthView)
+// user middleware (compute user and session and pass to routes)
+.macro({
+  auth: {
+    async resolve({ error, request: { headers } }) {
+      const session = await auth.api.getSession({
+        headers,
+      });
+
+      if (!session) return error(401);
+
+      return {
+        user: session.user,
+        session: session.session,
+      };
+    },
+  },
+})
+.get("/user", ({ user }) => user, {
+  auth: true,
+})
+.get("/", () => "Hello Elysia")
+.listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
